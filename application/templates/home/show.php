5632b465e09671a9459be2553c48c1f3dedcff12
James Patrick Keegan <atomicbomber.email@gmail.com>
2019-04-19 13:11:10 +0700

    Changed home/show title page in order to better reflect the page's purpose.

--- application/templates/home/show.php
+++ application/templates/home/show.php
@@ -1,6 +1,6 @@
 <?php ?>
 
-<?php $this->layout('shared/base-guest', ["title" => "Welcome"]) ?>
+<?php $this->layout('shared/base-guest', ["title" => "Daftar Menu"]) ?>
 
 <div style="margin: 0px 200px 0px 200px">
     <div id="app">

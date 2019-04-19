e7ba19e79b4d997ab9c5f922e7cc9ea0be2e9105
James Patrick Keegan <atomicbomber.email@gmail.com>
2019-04-19 13:10:29 +0700

    Added the sweetalert library so we can show confirmation modals in order-finishing and item-deletion actions.

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,12 +3,14 @@ import Vue from 'vue/dist/vue.esm'
 
 Vue.component("home", require("./components/Home.vue").default)
 
-
 try {
     window.Popper = require('popper.js').default;
     window.$ = window.jQuery = require('jquery');
     require('bootstrap');
 } catch (e) {}
+
+// Add SweetAlert
+window.swal = require("sweetalert")
 
 window.app = new Vue({
     el: '#app',
